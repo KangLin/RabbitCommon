4d3e3e82dc6a14d2038064f3453961d05aff20a6
KangLin <kl222@126.com>
2019-11-05 14:54:46 +0800

    Modify ci

--- ci/build-install-tools.sh
+++ ci/build-install-tools.sh
@@ -51,51 +51,23 @@ function install_android()
 {
     cd ${SOURCE_DIR}/Tools
     if [ ! -d "`pwd`/android-sdk" ]; then
+        ANDROID_STUDIO_VERSION=191.5900203
+        wget -c -nv https://dl.google.com/dl/android/studio/ide-zips/3.5.1.0/android-studio-ide-${ANDROID_STUDIO_VERSION}-linux.tar.gz
+        tar xzf android-studio-ide-${ANDROID_STUDIO_VERSION}-linux.tar.gz
+        export JAVA_HOME=`pwd`/android-studio/jre
+        export PATH=${JAVA_HOME}/bin:$PATH
         wget -c -nv https://dl.google.com/android/repository/sdk-tools-linux-4333796.zip
         mkdir android-sdk
         cd android-sdk
         cp ../sdk-tools-linux-4333796.zip .
-        unzip sdk-tools-linux-4333796.zip
+        unzip -q sdk-tools-linux-4333796.zip
         echo "Install sdk and ndk ......"
-        ./tools/bin/sdkmanager "platform-tools" "build-tools" "platforms;${ANDROID_API}" "ndk-bundle"
-        if [ ! -d ${SOURCE_DIR}/Tools/android-ndk ]; then
-            ln -s ${SOURCE_DIR}/Tools/android-sdk/ndk ${SOURCE_DIR}/Tools/android-ndk
-        fi
-    fi
-}
-
-function install_android_sdk_and_ndk()
-{
-    cd ${SOURCE_DIR}/Tools
-    
-    #下载android ndk  
-    if [ ! -d "`pwd`/android-ndk" ]; then
-        if [ "$QT_VERSION_DIR" = "5.9" ]; then
-            wget -c -nv http://dl.google.com/android/ndk/android-ndk-r10e-linux-x86_64.bin
-            chmod u+x android-ndk-r10e-linux-x86_64.bin
-            ./android-ndk-r10e-linux-x86_64.bin > /dev/null
-            mv android-ndk-r10e android-ndk
-            rm android-ndk-r10e-linux-x86_64.bin
-        else
-            NDK_VERSION=r20
-            wget -c -nv https://dl.google.com/android/repository/android-ndk-${NDK_VERSION}-linux-x86_64.zip
-            unzip android-ndk-${NDK_VERSION}-linux-x86_64.zip
-            mv android-ndk-${NDK_VERSION} android-ndk
-            rm android-ndk-${NDK_VERSION}-linux-x86_64.zip
-        fi
-    fi
-
-    cd ${SOURCE_DIR}/Tools
-
-    #Download android sdk  
-    if [ ! -d "`pwd`/android-sdk" ]; then
-        SDK_VERSION=r24.4.1
-        wget -c -nv https://dl.google.com/android/android-sdk_${SDK_VERSION}-linux.tgz
-        tar xf android-sdk_${SDK_VERSION}-linux.tgz 
-        mv android-sdk-linux android-sdk
-        rm android-sdk_${SDK_VERSION}-linux.tgz 
+        cd tools
         (sleep 5 ; while true ; do sleep 1 ; printf 'y\r\n' ; done ) \
-        | android-sdk/tools/android update sdk -u #-t tool,android-18,android-24,extra,platform,platform-tools,build-tools-28.0.3
+        | ./bin/sdkmanager "platform-tools" "build-tools;28.0.3" "platforms;${ANDROID_API}" "ndk-bundle"
+        if [ ! -d ${SOURCE_DIR}/Tools/android-ndk ]; then
+            ln -s ${SOURCE_DIR}/Tools/android-sdk/ndk-bundle ${SOURCE_DIR}/Tools/android-ndk
+        fi
     fi
 }
 
@@ -103,6 +75,8 @@ function function_android()
 {
     cd ${SOURCE_DIR}/Tools
     
+    sudo apt-get update -y -qq
+    #sudo apt-get install -qq -y openjdk-11-jdk
     # install oracle jdk
     #sudo add-apt-repository ppa:linuxuprising/java -y
     #sudo apt update
@@ -110,7 +84,7 @@ function function_android()
     
     #sudo apt install oracle-java11-set-default -qq -y
 
-    install_android_sdk_and_ndk
+    install_android
     
     sudo apt-get install ant -qq -y
     sudo apt-get install libicu-dev -qq -y
@@ -126,23 +100,17 @@ function function_unix()
 
     if [ "$BUILD_DOWNLOAD" != "TRUE"  ]; then
         #See: https://launchpad.net/~beineri
-        sudo add-apt-repository ppa:beineri/opt-qt${QT_VERSION}-`lsb_release -c|awk '{print $2}'` -y
+        sudo add-apt-repository ppa:beineri/opt-qt-${QT_VERSION}-`lsb_release -c|awk '{print $2}'` -y
     fi
 
     sudo apt-get update -y -qq
     sudo apt-get install debhelper fakeroot -y -qq
     sudo apt-get install -y -qq libglu1-mesa-dev \
-        libxkbcommon-x11-dev \
-        libpulse-mainloop-glib0 \
-        libgstreamer0.10-dev \
-        libgstreamer-plugins-base0.10-dev \
-        gstreamer1.0-pulseaudio \
-        libmysql-cil-dev libmysql-cil-dev libmysql-ocaml-dev libmysql++-dev libmysqld-dev libmysqlcppconn-dev
+        libxkbcommon-x11-dev
 
     if [ "$BUILD_DOWNLOAD" != "TRUE" ]; then
         sudo apt-get install -y -qq qt${QT_VERSION_DIR}base \
-            qt${QT_VERSION_DIR}tools \
-            qt${QT_VERSION_DIR}multimedia
+            qt${QT_VERSION_DIR}tools
         sed -i "s/export QT_VERSION=/export QT_VERSION=${QT_VERSION}/g" ${SOURCE_DIR}/debian/preinst
         sed -i "s/qt59/qt${QT_VERSION_DIR}/g" ${SOURCE_DIR}/debian/postinst
     fi
--- ci/build.sh
+++ ci/build.sh
@@ -13,9 +13,11 @@ if [ "$BUILD_TARGERT" = "android" ]; then
     export ANDROID_NDK_ROOT=${SOURCE_DIR}/Tools/android-ndk
     if [ -n "$APPVEYOR" ]; then
         export JAVA_HOME="/C/Program Files (x86)/Java/jdk1.8.0"
+        export ANDROID_NDK_ROOT=${SOURCE_DIR}/Tools/android-sdk/ndk-bundle
     fi
     if [ "$TRAVIS" = "true" ]; then
-        export JAVA_HOME=/usr/lib/jvm/java-8-openjdk-amd64
+        export JAVA_HOME=${SOURCE_DIR}/Tools/android-studio/jre
+        #export JAVA_HOME=/usr/lib/jvm/java-8-openjdk-amd64
     fi
     case $BUILD_ARCH in
         arm*)
@@ -26,6 +28,8 @@ if [ "$BUILD_TARGERT" = "android" ]; then
         ;;
     esac
     export PATH=${SOURCE_DIR}/Tools/apache-ant/bin:$JAVA_HOME:$PATH
+    export ANDROID_SDK=${ANDROID_SDK_ROOT}
+    export ANDROID_NDK=${ANDROID_NDK_ROOT}
 fi
 
 if [ "${BUILD_TARGERT}" = "unix" ]; then
@@ -89,10 +93,15 @@ esac
 
 if [ "${BUILD_TARGERT}" = "unix" ]; then
     cd $SOURCE_DIR
-	bash build_debpackage.sh ${QT_ROOT}
-	exit 0
+    if [ "${BUILD_DOWNLOAD}" != "TRUE" ]; then
+        sed -i "s/export QT_VERSION_DIR=.*/export QT_VERSION_DIR=${QT_VERSION_DIR}/g" ${SOURCE_DIR}/debian/postinst
+        sed -i "s/export QT_VERSION=.*/export QT_VERSION=${QT_VERSION}/g" ${SOURCE_DIR}/debian/preinst
+        cat ${SOURCE_DIR}/debian/postinst
+        cat ${SOURCE_DIR}/debian/preinst
+    fi
+    bash build_debpackage.sh ${QT_ROOT}
 
-    if [ "$TRAVIS_TAG" != "" -a "${QT_VERSION_DIR}" = "59" ]; then
+    if [ "$TRAVIS_TAG" != "" -a "${QT_VERSION_DIR}" = "512" ]; then
        export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:`pwd`/debian/rabbitcommon/opt/RabbitCommon/bin
        MD5=`md5sum ../rabbitcommon_*_amd64.deb|awk '{print $1}'`
        echo "MD5:${MD5}"
@@ -112,16 +121,27 @@ if [ -n "$GENERATORS" ]; then
     if [ -n "${STATIC}" ]; then
         CONFIG_PARA="-DBUILD_SHARED_LIBS=${STATIC}"
     fi
-
+    if [ -n "${ANDROID_ARM_NEON}" ]; then
+        CONFIG_PARA="${CONFIG_PARA} -DANDROID_ARM_NEON=${ANDROID_ARM_NEON}"
+    fi
     if [ "${BUILD_TARGERT}" = "android" ]; then
-	    cmake -G"${GENERATORS}" ${SOURCE_DIR} ${CONFIG_PARA} \
-		 -DCMAKE_INSTALL_PREFIX=`pwd`/android-build \
-		 -DCMAKE_VERBOSE=ON \
-		 -DCMAKE_BUILD_TYPE=Release \
-		 -DQt5_DIR=${QT_ROOT}/lib/cmake/Qt5 \
-         -DANDROID_PLATFORM=${ANDROID_API} \
-         -DANDROID_ABI="${BUILD_ARCH}" \
-         -DCMAKE_TOOLCHAIN_FILE=${ANDROID_NDK}/build/cmake/android.toolchain.cmake 
+        cmake -G"${GENERATORS}" ${SOURCE_DIR} ${CONFIG_PARA} \
+            -DCMAKE_INSTALL_PREFIX=`pwd`/android-build \
+            -DCMAKE_VERBOSE=ON \
+            -DCMAKE_BUILD_TYPE=Release \
+            -DQt5_DIR=${QT_ROOT}/lib/cmake/Qt5 \
+            -DQt5Core_DIR=${QT_ROOT}/lib/cmake/Qt5Core \
+            -DQt5Gui_DIR=${QT_ROOT}/lib/cmake/Qt5Gui \
+            -DQt5Widgets_DIR=${QT_ROOT}/lib/cmake/Qt5Widgets \
+            -DQt5Xml_DIR=${QT_ROOT}/lib/cmake/Qt5Xml \
+            -DQt5Sql_DIR=${QT_ROOT}/lib/cmake/Qt5Sql \
+            -DQt5Network_DIR=${QT_ROOT}/lib/cmake/Qt5Network \
+            -DQt5LinguistTools_DIR=${QT_ROOT}/lib/cmake/Qt5LinguistTools \
+            -DQt5AndroidExtras_DIR=${QT_ROOT}/lib/cmake/Qt5AndroidExtras \
+            -DANDROID_PLATFORM=${ANDROID_API} \
+            -DANDROID_ABI="${BUILD_ARCH}" \
+            -DCMAKE_MAKE_PROGRAM=make \
+            -DCMAKE_TOOLCHAIN_FILE=${ANDROID_NDK}/build/cmake/android.toolchain.cmake 
     else
 	    cmake -G"${GENERATORS}" ${SOURCE_DIR} ${CONFIG_PARA} \
 		 -DCMAKE_INSTALL_PREFIX=`pwd`/install \
@@ -140,14 +160,14 @@ else
     if [ "${BUILD_TARGERT}" = "android" ]; then
         ${QT_ROOT}/bin/qmake ${SOURCE_DIR} \
             "CONFIG+=release" ${CONFIG_PARA}
-        
+
         $MAKE
         $MAKE install INSTALL_ROOT=`pwd`/android-build
         ${QT_ROOT}/bin/androiddeployqt \
                        --gradle --verbose \
                        --input `pwd`/App/android-libRabbitCommonApp.so-deployment-settings.json \
                        --output `pwd`/android-build \
-                       #--android-platform ${ANDROID_API} 
+                       --android-platform ${ANDROID_API} 
                        
     else
         ${QT_ROOT}/bin/qmake ${SOURCE_DIR} \
@@ -161,10 +181,10 @@ else
 fi
 
 if [ "${BUILD_TARGERT}" = "windows_msvc" ]; then
-    if [ "${AUTOBUILD_ARCH}" = "x86" ]; then
+    if [ "${BUILD_ARCH}" = "x86" ]; then
         cp /C/OpenSSL-Win32/bin/libeay32.dll install/bin
         cp /C/OpenSSL-Win32/bin/ssleay32.dll install/bin
-    elif [ "${AUTOBUILD_ARCH}" = "x64" ]; then
+    elif [ "${BUILD_ARCH}" = "x64" ]; then
         cp /C/OpenSSL-Win64/bin/libeay32.dll install/bin
         cp /C/OpenSSL-Win64/bin/ssleay32.dll install/bin
     fi
