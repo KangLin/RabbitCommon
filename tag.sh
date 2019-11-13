90470e4e894fa47d2535c81e8a71d598b0a4d64d
KangLin <kl222@126.com>
2019-11-13 09:58:51 +0800

    Modify tag.sh

--- tag.sh
+++ tag.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
+set -e
 
 SOURCE_DIR=`pwd`
 
@@ -13,6 +14,7 @@ if [ -n "$1" ]; then
     echo "    - Test is ok ?"
     echo "    - Translation is ok ?"
     echo "    - Setup file is ok ?"
+    echo "    - Update_*.xml is ok ?"
     
     read -t 30 -p "Be sure to input Y, not input N: " INPUT
     if [ "$INPUT" != "Y" -a "$INPUT" != "y" ]; then
@@ -23,13 +25,13 @@ fi
 
 VERSION=`git describe --tags`
 if [ -z "$VERSION" ]; then
-    VERSION=` git rev-parse --short HEAD`
+    VERSION=`git rev-parse --short HEAD`
 fi
 
 sed -i "s/^\SET(BUILD_VERSION.*/\SET(BUILD_VERSION \"${VERSION}\")/g" ${SOURCE_DIR}/CMakeLists.txt
-
-APPVERYOR_VERSION="version: '${VERSION}.{build}'"
-sed -i "s/^version: '.*{build}'/${APPVERYOR_VERSION}/g" ${SOURCE_DIR}/appveyor.yml
+sed -i "s/export VERSION=.*/export VERSION=\"${VERSION}\"/g" ${SOURCE_DIR}/ci/build.sh
+sed -i "s/^version: '.*{build}'/version: '${VERSION}.{build}'/g" ${SOURCE_DIR}/appveyor.yml
+sed -i "s/export VERSION=.*/export VERSION=\"${VERSION}\"/g" ${SOURCE_DIR}/.travis.yml
 
 sed -i "s/^\    BUILD_VERSION=.*/\    BUILD_VERSION=\"${VERSION}\"/g" ${SOURCE_DIR}/App/App.pro
 sed -i "s/^\    BUILD_VERSION=.*/\    BUILD_VERSION=\"${VERSION}\"/g" ${SOURCE_DIR}/Src/Src.pro
@@ -38,6 +40,12 @@ sed -i "s/^\Standards-Version:.*/\Standards-Version:\"${VERSION}\"/g" ${SOURCE_D
 
 DEBIAN_VERSION=`echo ${VERSION}|cut -d "v" -f 2`
 sed -i "s/rabbitcommon (.*)/rabbitcommon (${DEBIAN_VERSION})/g" ${SOURCE_DIR}/debian/changelog
+sed -i "s/Version=.*/Version=${DEBIAN_VERSION}/g" ${SOURCE_DIR}/debian/RabbitCommon.desktop
+sed -i "s/[0-9]\+\.[0-9]\+\.[0-9]\+/${DEBIAN_VERSION}/g" ${SOURCE_DIR}/README*.md
+sed -i "s/[0-9]\+\.[0-9]\+\.[0-9]\+/${DEBIAN_VERSION}/g" ${SOURCE_DIR}/App/android/AndroidManifest.xml
+
+MAJOR_VERSION=`echo ${DEBIAN_VERSION}|cut -d "." -f 1`
+sed -i "s/android:versionCode=.*android/android:versionCode=\"${MAJOR_VERSION}\" android/g"  ${SOURCE_DIR}/App/android/AndroidManifest.xml
 
 if [ -n "$1" ]; then
     git add .
