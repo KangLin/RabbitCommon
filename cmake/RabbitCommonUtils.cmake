3a517c236f2aed806f0fdcc0afed6ad5ff5f57c0
Kang Lin <kl222@126.com>
2021-02-11 13:47:02 +0800

    Modify target properties in android

--- cmake/RabbitCommonUtils.cmake
+++ cmake/RabbitCommonUtils.cmake
@@ -396,18 +396,20 @@ function(ADD_TARGET)
             DESTINATION ${CMAKE_BINARY_DIR})
     endif()
 
-    if(DEFINED PARA_OUTPUT_DIR)
-        set_target_properties(${PARA_NAME} PROPERTIES
-            LIBRARY_OUTPUT_DIRECTORY ${PARA_OUTPUT_DIR}
-            RUNTIME_OUTPUT_DIRECTORY ${PARA_OUTPUT_DIR}
-            ARCHIVE_OUTPUT_DIRECTORY ${CMAKE_BINARY_DIR}/lib
-            )
-    else()
-        set_target_properties(${PARA_NAME} PROPERTIES
-            LIBRARY_OUTPUT_DIRECTORY ${CMAKE_BINARY_DIR}/bin
-            RUNTIME_OUTPUT_DIRECTORY ${CMAKE_BINARY_DIR}/bin
-            ARCHIVE_OUTPUT_DIRECTORY ${CMAKE_BINARY_DIR}/lib
-            )
+    if(NOT ANDROID)
+        if(DEFINED PARA_OUTPUT_DIR)
+            set_target_properties(${PARA_NAME} PROPERTIES
+                LIBRARY_OUTPUT_DIRECTORY ${PARA_OUTPUT_DIR}
+                RUNTIME_OUTPUT_DIRECTORY ${PARA_OUTPUT_DIR}
+                ARCHIVE_OUTPUT_DIRECTORY ${CMAKE_BINARY_DIR}/lib
+                )
+        else()
+            set_target_properties(${PARA_NAME} PROPERTIES
+                LIBRARY_OUTPUT_DIRECTORY ${CMAKE_BINARY_DIR}/bin
+                RUNTIME_OUTPUT_DIRECTORY ${CMAKE_BINARY_DIR}/bin
+                ARCHIVE_OUTPUT_DIRECTORY ${CMAKE_BINARY_DIR}/lib
+                )
+        endif()
     endif()
 
     # Be will to install header files
