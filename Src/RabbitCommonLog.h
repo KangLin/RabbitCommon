bf8764a1f0a88f9aed0d05e088e559dda838595b
KangLin <kl222@126.com>
2021-06-24 16:27:21 +0800

    Modify log

--- Src/RabbitCommonLog.h
+++ Src/RabbitCommonLog.h
@@ -46,38 +46,21 @@ private:
 #define LM_WARNING 2
 #define LM_ERROR 3
 
-#undef LOG_DEBUG
-#undef LOG_MODEL_DEBUG
-#undef LOG_ERROR
-#undef LOG_WARNING
-#undef LOG_INFO
-
 #ifdef DEBUG
-    #define LOG_DEBUG(...) 
     #define LOG_MODEL_DEBUG(model, ...)
 #else
     #ifdef HAVE_LOG4CPLUS
-        #define LOG_DEBUG(...) LOG4CPLUS_DEBUG_FMT(log4cplus::Logger::getRoot(), __VA_ARGS__)
         #define LOG_MODEL_DEBUG(model, ...) LOG4CPLUS_DEBUG_FMT(log4cplus::Logger::getInstance(model), __VA_ARGS__)
     #else
-        #define LOG_DEBUG(...) RabbitCommon::CLog::Instance()->Print(__FILE__, __LINE__, LM_DEBUG, "", __VA_ARGS__)
         #define LOG_MODEL_DEBUG(model, ...) RabbitCommon::CLog::Instance()->Print(__FILE__, __LINE__, LM_DEBUG, model, __VA_ARGS__)
     #endif
 #endif //#ifdef DEBUG
 
-#ifdef HAVE_LOG4CPLUS
-    #define LOG_ERROR(...) LOG4CPLUS_ERROR_FMT(log4cplus::Logger::getRoot(), __VA_ARGS__)
-    #define LOG_WARNING(...) LOG4CPLUS_WARN_FMT(log4cplus::Logger::getRoot(), __VA_ARGS__)
-    #define LOG_INFO(...) LOG4CPLUS_INFO_FMT(log4cplus::Logger::getRoot(), __VA_ARGS__)
-    
+#ifdef HAVE_LOG4CPLUS   
     #define LOG_MODEL_ERROR(model, ...) LOG4CPLUS_ERROR_FMT(log4cplus::Logger::getInstance(model), __VA_ARGS__)
     #define LOG_MODEL_WARNING(model, ...) LOG4CPLUS_WARN_FMT(log4cplus::Logger::getInstance(model), __VA_ARGS__)
     #define LOG_MODEL_INFO(model, ...) LOG4CPLUS_INFO_FMT(log4cplus::Logger::getInstance(model), __VA_ARGS__)
 #else
-    #define LOG_ERROR(...) RabbitCommon::CLog::Instance()->Print(__FILE__, __LINE__, LM_ERROR, "", __VA_ARGS__)
-    #define LOG_WARNING(...) RabbitCommon::CLog::Instance()->Print(__FILE__, __LINE__, LM_WARNING, "", __VA_ARGS__)
-    #define LOG_INFO(...) RabbitCommon::CLog::Instance()->Print(__FILE__, __LINE__, LM_INFO, "", __VA_ARGS__)
-    
     #define LOG_MODEL_ERROR(model, ...) RabbitCommon::CLog::Instance()->Print(__FILE__, __LINE__, LM_ERROR, model, __VA_ARGS__)
     #define LOG_MODEL_WARNING(model, ...) RabbitCommon::CLog::Instance()->Print(__FILE__, __LINE__, LM_WARNING, model, __VA_ARGS__)
     #define LOG_MODEL_INFO(model, ...) RabbitCommon::CLog::Instance()->Print(__FILE__, __LINE__, LM_INFO, model, __VA_ARGS__)
