56af46b721c04d7febfd312a50444019b4c012a4
Kang Lin <kl222@126.com>
2024-02-05 11:34:13 +0800

    Modify Install/MergeJsonFile.sh

--- Install/MergeJsonFile.sh
+++ Install/MergeJsonFile.sh
@@ -2,13 +2,13 @@
 
 # Use to merge update json file in linux
 # $1: the name of json file
-# $2: the json file of version
-# $3: the directory of json files
+# $2: the directory of json files
+# $3: the json file of version
 
 function usage()
 {
     echo "Usage:"
-    echo "$0 'the name of json file' 'the json file of version' 'the directory of json files'"
+    echo "$0 'the name of json file' 'the directory of json files' 'the json file of version'"
     exit
 }
 
@@ -16,18 +16,18 @@ update_json_file=$1
 if [ -z $update_json_file ]; then
     update_json_file="update.json"
 fi
-version_json_file=$2
-if [ -z $version_json_file ]; then
-    version_json_file="version.json"
-fi
-if [ ! -f $version_json_file ]; then
-    usage
-fi
-directory_json_file=$3
+directory_json_file=$2
 if [ -z $directory_json_file ]; then
     directory_json_file=`pwd`
 fi
 if [ ! -d $directory_json_file ]; then
+    usage
+fi
+version_json_file=$3
+if [ -z $version_json_file ]; then
+    version_json_file="${directory_json_file}/version.json"
+fi
+if [ ! -f $version_json_file ]; then
     usage
 fi
 
@@ -41,7 +41,7 @@ index=0
 for file in `ls *.json`
 do
     echo $file
-    if [ $file == $version_json_file ]; then
+    if [ $directory_json_file/$file == $version_json_file ]; then
         continue
     fi
     if [ $file == $update_json_file ]; then
