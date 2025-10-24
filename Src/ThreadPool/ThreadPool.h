555fe0598cedffc3aff482f978fb278ea23f4023
Kang Lin <kl222@126.com>
2025-10-24 13:12:32 +0800

    Add thread pool

---
+++ Src/ThreadPool/ThreadPool.h
@@ -0,0 +1,51 @@
+// Copyright Copyright (c) Kang Lin studio, All Rights Reserved
+// Author Kang Lin <kl222@126.com>
+
+#pragma once
+#include <QVector>
+#include <QThread>
+#include <QMutex>
+#include "Worker.h"
+
+/*!
+ * \brief The CThreadPool class
+ */
+class RABBITCOMMON_EXPORT CThreadPool : public QObject
+{
+    Q_OBJECT
+
+public:
+    explicit CThreadPool(QObject *parent = nullptr);
+    virtual ~CThreadPool() override;
+
+    /*! Start pool with N workers
+     *  \param cb: create worker callback function. \see CWorker *CreateWorkerDownload()
+     *  \param maxThreadCount: Max thread counts
+     */
+    virtual int Start(std::function<CWorker*()> cb,
+                      int maxThreadCount = QThread::idealThreadCount()/*std::thread::hardware_concurrency()*/);
+    //! Stop all workers and clean up
+    virtual int Stop();
+
+    //! Choose worker
+    virtual CWorker* ChooseWorker();
+
+Q_SIGNALS:
+    void sigMessage(const QString &message);
+    void sigError(int nErr, const QString &szError = QString());
+    void sigRunning();
+    void sigFinished(bool success = true, const QString &message = QString());
+
+protected:
+    enum class State{
+        Stop,
+        Start,
+        Running
+    };
+    State m_State;
+    QVector<QThread*> m_Threads;
+    QVector<CWorker*> m_Workers;
+    QMutex m_MutexWorkers;
+    int m_nMaxThreadCount;
+    int m_nNextId;
+};
