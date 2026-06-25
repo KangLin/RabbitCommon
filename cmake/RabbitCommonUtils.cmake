d4c2511c90ff1eba5828dedb63e46276e85424e2
Kang Lin <kl222@126.com>
2026-06-25 16:36:00 +0800

    Fix android build directory

--- cmake/RabbitCommonUtils.cmake
+++ cmake/RabbitCommonUtils.cmake
@@ -1147,21 +1147,17 @@ function(ADD_TARGET)
                 message("QT_ANDROID_EXTRA_LIBS: ${VAR_QT_ANDROID_EXTRA_LIBS}")
             endif()
 
-            if(QT_VERSION GREATER_EQUAL 6.11.0)
-                set(ANDROID_BUILD android-build-${PARA_NAME})
-            else()
-                set(ANDROID_BUILD android-build)
-            endif()
-            process_build_gradle(${PARA_NAME} ${CMAKE_CURRENT_BINARY_DIR}/${ANDROID_BUILD})
+            set(ANDROID_BUILD ${CMAKE_CURRENT_BINARY_DIR}/android-build)
+            process_build_gradle(${PARA_NAME} ${ANDROID_BUILD})
 
             # NOTE: 如果不用 ADD_TARGET 时，请手动安装.参见: INSTALL_DIR, INSTALL_FILE
 #                file(COPY ${CMAKE_BINARY_DIR}/assets
-#                    DESTINATION ${CMAKE_CURRENT_BINARY_DIR}/${ANDROID_BUILD}/)
+#                    DESTINATION ${ANDROID_BUILD}/)
             add_custom_command(
                 TARGET ${PARA_NAME} POST_BUILD
-                COMMAND ${CMAKE_COMMAND} -E echo "copy directory ${CMAKE_BINARY_DIR}/assets to ${CMAKE_CURRENT_BINARY_DIR}/${ANDROID_BUILD}/assets"
-                COMMAND ${CMAKE_COMMAND} -E copy_directory "${CMAKE_BINARY_DIR}/assets" "${CMAKE_CURRENT_BINARY_DIR}/${ANDROID_BUILD}/assets"
-                COMMENT "Copy directory ${CMAKE_BINARY_DIR}/assets/ to ${CMAKE_CURRENT_BINARY_DIR}/${ANDROID_BUILD}/assets"
+                COMMAND ${CMAKE_COMMAND} -E echo "copy directory ${CMAKE_BINARY_DIR}/assets to ${ANDROID_BUILD}/assets"
+                COMMAND ${CMAKE_COMMAND} -E copy_directory "${CMAKE_BINARY_DIR}/assets" "${ANDROID_BUILD}/assets"
+                COMMENT "Copy directory ${CMAKE_BINARY_DIR}/assets/ to ${ANDROID_BUILD}/assets"
                 COMMAND_EXPAND_LISTS
                 VERBATIM)
 
